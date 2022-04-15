58a03168f26347e642ad56a376f12743f48032b0
pavelvasilev <pavelvasilev2304@gmail.com>
2022-04-15 19:47:57 -0400

    i1

--- api_control.py
+++ api_control.py
@@ -28,20 +28,20 @@ async def getUser(user_id):
         url = prepare_url('api/users/' + str(user_id))
         async with session.get(url) as response:
             if response.status != 200:
-                raise ValueError(response.status)
+                raise ValueError("STATUS CODE: {}".format(response.status))
             response = await response.json()
     return response
 
 
 async def createUser(name, email, **args):
-    params = {'name': name, 'password': password, 'email': email}
+    params = {'name': name, 'email': email}
     for key in args.keys():
         params[key] = args[key]
     async with aiohttp.ClientSession() as session:
         url = prepare_url('api/users')
         async with session.post(url, json=params) as response:
             if response.status != 200:
-                raise ValueError(response.status)
+                raise ValueError("STATUS CODE: {}".format(response.status))
             response = await response.json()
     return response
 
@@ -51,13 +51,13 @@ async def deleteUser(user_id):
         url = prepare_url('api/users/' + str(user_id))
         async with session.delete(url) as response:
             if response.status != 200:
-                raise ValueError(response.status)
+                raise ValueError("STATUS CODE: {}".format(response.status))
             response = await response.json()
     return response
 
 
 async def changeUser(user_id, name, email, **args):
-    params = {'name': name, 'password': password, 'email': email}
+    params = {'name': name, 'email': email}
     for key in args.keys():
         params[key] = args[key]
     async with aiohttp.ClientSession() as session:
@@ -65,7 +65,7 @@ async def changeUser(user_id, name, email, **args):
         async with session.put(url, json=params) as response:
             print(url)
             if response.status != 200:
-                raise ValueError(response.status)
+                raise ValueError("STATUS CODE: {}".format(response.status))
             response = await response.json()
     return response
             
