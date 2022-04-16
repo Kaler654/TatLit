98adcbd141b932fab1f48a504d9b1e6e30d4926e
pavelvasilev <pavelvasilev2304@gmail.com>
2022-04-16 00:56:58 -0400

    i

--- api_control.py
+++ api_control.py
@@ -6,7 +6,7 @@ URL = "http://localhost:8080"
 
 
 def prepare_url(path):
-    dest = URL + path
+    dest = URL + '/' + path
     if dest.count('//') > 1:
         raise("Invalid URL")
     dest = dest[:-1] if dest.endswith('/') else dest
@@ -63,9 +63,65 @@ async def changeUser(user_id, name, email, **args):
     async with aiohttp.ClientSession() as session:
         url = prepare_url('api/users/' + str(user_id)) 
         async with session.put(url, json=params) as response:
-            print(url)
             if response.status != 200:
                 raise ValueError("STATUS CODE: {}".format(response.status))
             response = await response.json()
     return response
+
+
+async def getUserWords(user_id):
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/words/' + str(user_id))
+        async with session.get(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def getWords():
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/words/')
+        async with session.get(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def sendWord(word, translation):
+    params = {'word': word, 'word_ru': translation}
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/words/') 
+        async with session.post(url, json=params) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def deleteWord(word_id):
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/words/' + str(word_id)) 
+        async with session.delete(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def changeWord(word_id, word, translation):
+    params = {'word': word, 'word_ru': translation}
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/words/' + str(word_id))
+        async with session.put(url, json=params) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    print(loop.run_until_complete(getUserWords(1)))
             
