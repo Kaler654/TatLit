a68b4a72b313cc3f2f7a7db390a7cd26816b799d
pavelvasilev <pavelvasilev2304@gmail.com>
2022-04-16 04:11:08 -0400

    save

--- api_control.py
+++ api_control.py
@@ -89,7 +89,7 @@ async def getWords():
     return response
 
 
-async def sendWord(word, translation):
+async def createWord(word, translation):
     params = {'word': word, 'word_ru': translation}
     async with aiohttp.ClientSession() as session:
         url = prepare_url('api/words/') 
@@ -121,7 +121,68 @@ async def changeWord(word_id, word, translation):
     return response
 
 
+async def changeWordLevel(user_id, word_id, word_level, date):
+    params = {'user_id': user_id, 'word_id': word_id, 'word_level': word_level, 'date': date}
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/word_levels/' + str(user_id) + '/' + str(word_id))
+        async with session.put(url, json=params) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def getWordLevels():
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/word_levels/')
+        async with session.get(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def getWordLevel(word_level_id):
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/word_levels/' + str(word_level_id))
+        async with session.get(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def createWordLevel(user_id, word_id, word_level_id, date=None):
+    params = {'user_id': user_id, 'word_id': word_id, 'word_level_id': word_level_id, 'date': date}
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/word_levels')
+        async with session.post(url, params=params) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def deleteWordLevel(word_level_id):
+    async with aiohttp.ClientSession() as session:
+        url = prepare_url('api/word_levels/' + str(word_level_id))
+        async with session.delete(url) as response:
+            if response.status != 200:
+                raise ValueError("STATUS CODE: {}".format(response.status))
+            response = await response.json()
+    return response
+
+
+async def getWordLevel2(user_id, word_id):
+    response = getWordLevels()
+    for key in response.keys():
+        way = response[key]
+        if way['word_id'] == word_id and way['user_id'] == user_id:
+            return {key: way}
+    return {'error': 'Not found'}
+
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    print(loop.run_until_complete(getUserWords(1)))
+    print(loop.run_until_complete(getWords()))
             
